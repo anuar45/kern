7c2d90b713dcbb0cc0febc9da3336fc6e9f4b73b
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-03-20 19:56:31 +0100

    dup2 handover

--- ch1/dup2.go
+++ ch1/dup2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -12,15 +13,14 @@ func main() {
 	lineIndex := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, lineIndex)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Println(os.Stderr, "dup2: %v\n", err)
 			}
-			countLines(f, counts)
-			indexFile(f, lineIndex)
+			countLines(f, counts, lineIndex)
 			f.Close()
 		}
 
@@ -30,24 +30,12 @@ func main() {
 	}
 }
 
-func countLines(f *os.File, count map[string]int) {
+func countLines(f *os.File, count map[string]int, lineIndex map[string]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		count[input.Text()]++
-	}
-}
-
-func indexFile(f *os.File, lineIndex map[string]string) {
-	fmt.Println("test")
-	input := bufio.NewScanner(f)
-	fmt.Println(input.Scan())
-	for input.Scan() {
-		fmt.Println(3, lineIndex[input.Text()])
 		if !strings.Contains(lineIndex[input.Text()], f.Name()) {
-			fmt.Println(1, f.Name())
-			fmt.Println(2, input.Text())
-			lineIndex[input.Text()] += f.Name()
-			fmt.Println(3, lineIndex[input.Text()])
+			lineIndex[input.Text()] += filepath.Base(f.Name())
 		}
 	}
 }
