515cfeddc87420270181b394673b5b7413425aeb
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-03-26 12:41:37 +0100

    fetch17 done

--- ch1/fetch17.go
+++ ch1/fetch17.go
@@ -14,13 +14,11 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		var b io.Writer
-		_, err = io.Copy(b, resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
--- ch1/mysrv.go
+++ ch1/mysrv.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
+}
 func main() {
-	http.HandleFunc()
+	http.HandleFunc("/", home)
+	http.ListenAndServe("127.0.0.1:8888", nil)
 }
