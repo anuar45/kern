84ac7aae6a572014951dfdcbb16a913caa8565c4
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-05-19 20:08:19 +0200

    surface 4 done

---
+++ ch3/surface4/surface4.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"net/http"
+	"strconv"
+)
+
+type svgParams struct {
+	xyrange float64
+	xyscale float64
+	zscale  float64
+	height  int
+	width   int
+	cells   int
+	angle   float64
+	sin30   float64
+	cos30   float64
+}
+
+func main() {
+
+	http.HandleFunc("/surface", handler)
+	http.ListenAndServe("localhost:8080", nil)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	height, _ := strconv.Atoi(r.Form["h"][0])
+	width, _ := strconv.Atoi(r.Form["w"][0])
+
+	s := newSvgParams(height, width)
+
+	svg := getSvg(s)
+	w.Header().Set("Content-Type", "image/svg+xml")
+	fmt.Fprintf(w, svg)
+}
+
+func newSvgParams(height, width int) svgParams {
+	xyrange := 30.0
+	xyscale := float64(width) / 2.0 / xyrange
+	zscale := float64(height) * 0.4
+	angle := math.Pi / 6
+	sin30 := math.Sin(angle)
+	cos30 := math.Cos(angle)
+
+	s := svgParams{
+		xyrange: xyrange,
+		xyscale: xyscale,
+		zscale:  zscale,
+		height:  height,
+		width:   width,
+		cells:   100,
+		angle:   math.Pi / 6,
+		sin30:   sin30,
+		cos30:   cos30,
+	}
+
+	return s
+}
+
+func getSvg(s svgParams) string {
+	var svg bytes.Buffer
+
+	svg.WriteString(fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; f ill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>\n", s.width, s.height))
+
+	for i := 0; i < s.cells; i++ {
+		for j := 0; j < s.cells; j++ {
+			ax, ay := corner(s, i+1, j)
+			bx, by := corner(s, i, j)
+			cx, cy := corner(s, i, j+1)
+			dx, dy := corner(s, i+1, j+1)
+			svg.WriteString(fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g '/>\n", ax, ay, bx, by, cx, cy, dx, dy))
+		}
+	}
+
+	svg.WriteString(fmt.Sprintln("</svg>"))
+
+	return svg.String()
+}
+
+func corner(s svgParams, i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i, j).
+	x := s.xyrange * (float64(i)/float64(s.cells) - 0.5)
+	y := s.xyrange * (float64(j)/float64(s.cells) - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx, sy).
+	sx := float64(s.width)/2.0 + (x-y)*s.cos30*s.xyscale
+	sy := float64(s.height)/2.0 + (x+y)*s.sin30*s.xyscale - z*s.zscale
+
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
