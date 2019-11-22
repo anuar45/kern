ed0ac9b47127fbe3e497439111afa4e689f69ae2
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-11-22 18:31:40 +0100

    reverse1 done

---
+++ ch4/reverse2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl := [5]int{1, 2, 3, 4, 5}
+
+	reverse(&sl)
+
+	fmt.Println(sl)
+}
+
+func reverse(sl *[5]int) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
