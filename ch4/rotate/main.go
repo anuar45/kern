e45711fc93b5b8cf9123f759ad440ae525501b5e
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-11-25 19:00:03 +0100

    rotate done

--- ch4/rotate/main.go
+++ ch4/rotate/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(rotate([]int{55, 66, 77, 88, 99}, 1))
 }
 
-func rotate() []int {
+func rotate(nums []int, n int) []int {
+	r := nums[n:]
+	for i := 0; i < len(nums[:n]); i++ {
+		r = append(r, nums[i])
+	}
 
+	return r
 }
